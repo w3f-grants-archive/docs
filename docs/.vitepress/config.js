39d7d29e74456bedcb7bb74dacb40d0eaa6e2cee
Dusan Morhac <55763425+dudo50@users.noreply.github.com>
2023-01-14 11:52:42 +0100

    set base

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,11 +1,12 @@
 export default {
     title: 'ParaSpell',
     description: 'Make world more connected with XCM.',
+    base: '/docs/',
     head: [
-    ['link', { rel: "icon", sizes: "180x180", href: "./docs/img/favicon.png"}],
+    ['link', { rel: "icon", sizes: "180x180", href: "./img/favicon.png"}],
   ],
     themeConfig: {
-        logo: '/docs/img/paraspell.png',
+        logo: './img/paraspell.png',
 
         socialLinks: [
             { icon: 'github', link: 'https://github.com/paraspell' },
@@ -13,36 +14,36 @@ export default {
         ],
 
         nav: [
-            { text: 'Home', link: '/docs/'},
+            { text: 'Home', link: '/'},
         ],
         sidebar: [
           {
             items: [
-                { text: 'About ParaSpell✨', link: '/docs' },
-                { text: 'Getting Started', link: './docs/getting-started' }
+                { text: 'About ParaSpell✨', link: '/' },
+                { text: 'Getting Started', link: './getting-started' }
             ]
           },
           {
             text: 'Pallets in SDK',
             items: [
-              { text: 'Asset Pallet', link: './docs/sdk/AssetPallet' },
-              { text: 'HRMP Pallet', link: './docs/sdk/HRMP' },
-              { text: 'XCM Pallet', link: './docs/sdk/xcmPallet' },
-              { text: 'XYK Pallet', link: './docs/sdk/XYK' }
+              { text: 'Asset Pallet', link: './sdk/AssetPallet' },
+              { text: 'HRMP Pallet', link: './sdk/HRMP' },
+              { text: 'XCM Pallet', link: './sdk/xcmPallet' },
+              { text: 'XYK Pallet', link: './sdk/XYK' }
             ]
           },
           {
             text: 'Usage of UI',
             items: [
-              { text: 'Starting local network', link: './docs/ui/local' },
-              { text: 'Modifying local network', link: './docs/ui/modify' },
-              { text: 'Adding new currency to UI', link: './docs/ui/currency' }
+              { text: 'Starting local network', link: './ui/local' },
+              { text: 'Modifying local network', link: './ui/modify' },
+              { text: 'Adding new currency to UI', link: './ui/currency' }
             ]
           },
           {
             text: 'Interesting links',
             items: [
-              { text: 'Links', link: './docs/links' },
+              { text: 'Links', link: './links' },
             ]
           },
         ]
